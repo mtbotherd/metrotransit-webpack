20e0a6bc4ca5e7937ef32a4da0c363819df0998f
Rich Bothe <rich.bothe@metrotransit.org>
2020-01-14 16:39:43 -0600

    Webpack configuration progress.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import _ from "lodash";
-import "./scss/styles.scss";
+import _ from 'lodash';
 
 function component() {
-  const element = document.createElement("div");
+  const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work.
-  element.innerHTML = _.join(["hello", "webpack"], " ");
-  element.classList.add("hello");
+  element.innerHTML = _.join(['hello', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,85 @@
-const path = require("path");
+// Imports dependancies
+const path = require('path');
+require ('@babel/register');
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          "style-loader", // creates style nodes from JS strings
-          "css-loader", // translates CSS into common JS
-          "sass-loader" // compiles Sass to CSS, using Node Sass by default
-        ]
-      }
-    ]
-  }
-};
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+// Webpack configuration
+const config = {
+
+	// Mode
+	//mode: 'production',
+
+	// Entry
+	entry: {
+		app: './src/index.js'
+	},
+
+	// Loaders
+	module: {
+		rules: [
+
+			// JS files
+			{
+				test: /\.js$/,
+				exclude: '/node_modules',
+				use: {
+					loader: 'babel-loader'
+				}
+
+			},
+
+			// CSS files
+			{
+				test: /\.scss$/,
+				exclude: '/node_modules',
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			}
+		]
+	},
+
+	// Plugins
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Metro Transit with Webpack',
+			filename: 'index.html',
+			meta: {
+				viewport: 'width=device-width, initial-scale=1'
+			},
+			publicPath: '/dist',
+			hash: true // Useful for cache busting
+		})
+	],
+
+	// Reload on file change
+	watch: true,
+	watchOptions: {
+		ignored: '/node_modules'
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+		hot: true
+	},
+
+	// Output
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	}
+}
+
+// Exports
+module.exports = config;
