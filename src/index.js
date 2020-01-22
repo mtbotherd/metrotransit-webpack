fce33c95f56672f0821623815bccaa64e9754550
Rich Bothe <rich.bothe@metrotransit.org>
2020-01-22 07:37:45 -0600

    Adding images

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-import _ from 'lodash';
-require('./scss/styles.scss');
+import 'bootstrap'
+import './scss/app.scss'
 
-function component() {
-  const element = document.createElement('div');
-  const btn = document.createElement('button');
-
-  // Lodash, currently included via a script, is required for this line to work.
-  element.innerHTML = _.join(['hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
-}
-
-document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,109 +1,133 @@
-// Imports dependancies
-const path = require('path');
-require ('@babel/register');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CompressionPlugin = require('compression-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
+const path = require("path");
+const globSync = require("glob").sync;
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-// Webpack configuration
-const config = {
-
-	// Mode
-	//mode: 'production',
-
-	// Entry
-	entry: {
-		app: './src/index.js',
-		splitchunk: './src/splitchunk.js'
-	},
-
-	// Loaders
-	module: {
-		rules: [
-
-			// JS files
-			{
-				test: /\.js$/,
-				exclude: '/node_modules',
-				use: {
-					loader: 'babel-loader'
-				}
-
-			},
-
-			// CSS files
-			{
-				test: /\.scss$/,
-				exclude: '/node_modules',
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
-			},
-
-			// Image files
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader',
-				],
-			},
-
-			// Font files
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader'
-				]
-			}
-		]
-	},
-
-	// Plugins
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			title: 'Metro Transit with Webpack',
-			filename: 'index.html',
-			meta: {
-				viewport: 'width=device-width, initial-scale=1'
-			},
-			publicPath: '/dist',
-			hash: true // Useful for cache busting
-		})
-	],
-
-	// Reload on file change
-	watch: true,
-	watchOptions: {
-		ignored: 'node_modules'
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		contentBase: './dist',
-		hot: true
-	},
-
-	// Output
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: '[name].bundle.js'
-	},
-
-	// Optimize
-	optimazation: {
-		splitChunks: {
-			chunks: 'all'
-		}
-	}
-}
-
-// Exports
-module.exports = config;
+module.exports = (env, options) => ({
+  entry: ["./src/index.js"],
+  devServer: {
+    contentBase: "./dist"
+  },
+  devtool: "source-map",
+  node: {
+    fs: 'empty'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          options.mode !== "production"
+            ? "style-loader"
+            : {
+                loader: MiniCssExtractPlugin.loader,
+                options: {
+                  publicPath: "../"
+                }
+              },
+          "css-loader",
+          {
+            loader: 'postcss-loader', 
+            options: {
+              plugins: function () {
+                return [
+                  require('precss'),
+                  require('autoprefixer')
+                ];
+              }
+            }
+          },
+          "sass-loader"
+        ]
+	  },
+	  {
+        test: /\.(woff|woff2|eot|ttf|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "font/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "img/"
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(html)$/,
+        use: {
+          loader: "html-srcsets-loader",
+          options: {
+            attrs: [":src", ':srcset']
+          }
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css"
+    }),
+    new CleanWebpackPlugin(["dist"]),
+    ...globSync("src/**/*.html").map(fileName => {
+      return new HtmlWebpackPlugin({
+        template: fileName,
+        inject: "body",
+        filename: fileName.replace("src/", "")
+      });
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      Popper: ["popper.js", "default"],
+      Util: "exports-loader?Util!bootstrap/js/dist/util",
+      Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
+    })
+  ],
+  optimization: {
+    minimizer: [
+        new UglifyJsPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: false,
+            extractComments: false
+        }),
+        new CompressionPlugin({
+            test: /\.js$|\.css(\?.*)?$/i
+        }),
+        new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: ""
+  }
+});
